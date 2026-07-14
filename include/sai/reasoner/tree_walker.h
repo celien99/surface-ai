2383b2acc5ecfb211a31a53279138dab575a0c57
杨宇航 <y17770372709@outlook.com>
2026-07-14 13:15:19 +0800

    chore(rule,reasoner): 🔧 删除残留的占位头文件（实际实现在 src/ 内部）

--- include/sai/reasoner/tree_walker.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace sai::reasoner {
-// Task 2 will fill this in
-}  // namespace sai::reasoner
--- include/sai/rule/conflict_resolver.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace sai::rule {
-// Task 2 will fill this in
-}  // namespace sai::rule
